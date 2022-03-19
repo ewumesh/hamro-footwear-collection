976ba603472a969c2ab9c1ca1da2f691de144e02
Prajwol7 <Prajwolaryal7@gmail.com>
2022-03-19 14:26:05 +0545

    update

---
+++ src/routes/HomeRoutes.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+
+// Layout Types
+import BaseLayout from "../layouts/BaseLayout";
+
+// Route Views
+import Home from "../views/Home/HomeContainer";
+import SingleProductContainer from "../views/Product/SingleProductContainer";
+import CategoryContainer from "../views/Category/CategoryContainer";
+
+var routes = [
+  {
+    path: "/fashion-cube",
+    exact: true,
+    layout: BaseLayout,
+    component: Home,
+  },
+
+  {
+    path: "/home",
+    layout: BaseLayout,
+    component: () => <Redirect to="/fashion-cube" />,
+  },
+  {
+    path: "/fashion-cube/single-product/:id",
+    layout: BaseLayout,
+    component: SingleProductContainer,
+  },
+  {
+    path: "/fashion-cube/shops/:category",
+    layout: BaseLayout,
+    component: CategoryContainer,
+  },
+];
+
+export default routes;
